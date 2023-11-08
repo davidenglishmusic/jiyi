17196f40d0eb06030cd9e757df182665d0259ec5
davidenglishmusic <davidenglishcodes@gmail.com>
2023-11-07 21:04:23 -0500

    Fix vocab

--- db/seeds.rb
+++ db/seeds.rb
@@ -151,7 +151,7 @@
   { id: 122, original: '今天是 9 月 1 日.', pronunciation: 'Jīntiān shì 9 yuè 1 rì.', meaning: 'Today is September 1.' },
   { id: 123, original: '今天星期几?', pronunciation: 'Jīntiān xīngqí jǐ?', meaning: 'What day is it today?' },
   { id: 124, original: '星期三.', pronunciation: 'Xīngqísān.', meaning: 'It is Wednesday.' },
-  { id: 125, original: '昨天是几号?', pronunciation: 'Zuótiān shì jǐ hào?', meaning: 'week' },
+  { id: 125, original: '昨天是几号?', pronunciation: 'Zuótiān shì jǐ hào?', meaning: 'What day was it yesterday?' },
   { id: 126, original: '昨天是 8 月 31 日星期二.', pronunciation: "Zuótiān shì 8 yuè 31 rì xīngqí'èr.",
     meaning: 'Yesterday was Tuesday, August 31st.' },
   { id: 127, original: '明天呢?', pronunciation: 'Míngtiān ne?', meaning: 'And tomorrow?' },
