ceb76048a07e54f5e529f97c10fd76e231b2a725
davidenglishmusic <davidenglishcodes@gmail.com>
2023-11-11 20:08:34 -0500

    Stay in current lesson group

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -9,12 +9,12 @@ class TranslationsController < ApplicationController
     @translation = Translation.find(params[:id])
     next_id = params[:id].to_i + 1
     prev_id = params[:id].to_i - 1
-    @next_url = if Translation.find_by(id: next_id)
+    @next_url = if Translation.find_by(id: next_id, lesson_id: @translation.lesson_id)
                   translation_path(id: next_id)
                 else
                   root_path
                 end
-    @prev_url = if Translation.find_by(id: prev_id)
+    @prev_url = if Translation.find_by(id: prev_id, lesson_id: @translation.lesson_id)
                   translation_path(id: prev_id)
                 else
                   root_path
